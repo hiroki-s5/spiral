cce2134e659421d1402dff00a21e43a848971686
Yamada Hiroki <hiroki@yamadanoMacBook-Air.local>
2026-03-12 21:29:39 +0900

    v5.0.5

---
+++ stealth-preload.js
@@ -0,0 +1,5 @@
+;(function(){
+  try{Object.defineProperty(navigator,'webdriver',{get:()=>undefined,configurable:true});}catch(e){}
+  try{if(!window.chrome)window.chrome={app:{isInstalled:false},runtime:{},csi:function(){},loadTimes:function(){}};}catch(e){}
+  try{Object.defineProperty(navigator,'languages',{get:()=>['ja-JP','ja','en-US','en'],configurable:true});}catch(e){}
+})();
